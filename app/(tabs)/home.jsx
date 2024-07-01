44f356bda1f71e3fdae922db4ff6f249acc6831d
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2024-07-01 20:03:31 +0800

    Implementing Trending Section in Home Page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,12 +13,13 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false);
 
@@ -60,7 +61,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending posts={[] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
