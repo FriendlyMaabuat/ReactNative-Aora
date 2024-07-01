e9ee83bded2249912ffe5189d1482f96ead11ddb
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2024-07-01 20:36:22 +0800

    Implementing the Search by Title function

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,29 +1,42 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
 import React, { useState } from "react";
 
 import { icons } from "../constants";
+import { usePathname, router } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-  const [showPassword, setshowPassword] = useState(false);
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
       <TextInput
         className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={value}
+        value={query}
         placeholderTextColor="#CDCDE0"
         placeholder="Search for a video topic"
-        onChangeText={handleChangeText}
-        secureTextEntry={title === "Password" && !showPassword}
+        onChangeText={(e) => setQuery(e)}
       />
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (!query) {
+            return Alert.alert(
+              "Missing query",
+              "Please input something to search results across the database"
+            );
+          }
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
